1ab008965935c31571cfd7dffb6f1cac74c55abd
Sunju <57226409+sunjus@users.noreply.github.com>
2023-02-07 23:47:58 +0900

    Add skills feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
+import Skills from './components/Skills';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
