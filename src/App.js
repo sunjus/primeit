93aaf6ff831b58ac14e450b098dc2f1f1e761a2f
Sunju <57226409+sunjus@users.noreply.github.com>
2023-02-08 17:00:59 +0900

    implement project feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
-
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
