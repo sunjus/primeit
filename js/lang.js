88683e7e82d341b7fa2131f65f0e70b6a1390735
Sunju <57226409+sunjus@users.noreply.github.com>
2023-04-28 14:28:43 +0900

    Add lang trans

---
+++ js/lang.js
@@ -0,0 +1,211 @@
+const langArr = {
+    "home": {
+        "kr": "프라임잇",
+        "en": "PRIME IT"
+    },
+    "home2": {
+        "kr": "크리에이티브 컨설팅",
+        "en": "Creative Consulting"
+    },
+    "title1": {
+        "kr": "우리는 불가능을",
+        "en": "We turn impossible tasks"
+    },
+    "title2": {
+        "kr": "가능하게 만듭니다",
+        "en": "into possible"
+    },
+    "ref": {
+        "kr": "레퍼런스 프로젝트",
+        "en": "Reference Projects"
+    },
+    "overview": {
+        "kr": "개요",
+        "en": "Overview"
+    },
+    "overview-des": {
+        "kr": "Prime IT Oy는 긍정적이고 지속적인 변화를 제공하도록 설계되었습니다. 우리는 고객이 장기 지속가능한 수익을 얻도록 고유한 전문성과 주도적인 실행력을 결합합니다. 2007년 설립된 이래, Prime IT Oy는 전 세계 기업들이 최고 수준의 국제 수준에서 협상을 수행하고 금융 및 정치계의 전력 네트워크 지원을 받아 주요 거래를 성사할 수 있도록 돕고 있습니다. Big picture Thinking을 통해 그린필드 프로젝트를 포함하여 뛰어난 비즈니스 개발 및 투자 시나리오를 위한 토대를 마련할 수 있습니다. 우리는 모든 책임을 지는 것을 결코 두려워하지 않으며 올바른 결과를 얻을 때까지 항상 모든 도전을 성사시킵니다.",
+        "en": "Prime IT Oy is designed to deliver positive, enduring change. We combine unique expertise and proactive practices to achieve long-lasting gains for our clients. Since our formation in 2007, Prime IT Oy has been helping businesses all over the world to conduct negotiations on the highest international level and conclude major deals with the support of our power network in financial and political circles. Big-picture thinking allows us to do the groundwork for outstanding business development and investment scenarios, including greenfield projects. We never fear to take full responsibility and always go through with every challenge until we get the right result."
+    },
+    "overview-title1": {
+        "kr": "산업",
+        "en": "Industries"
+    },
+    "overview-des1": {
+        "kr": "우리는 다양한 산업 분야를 위해 개발된 포괄적인 자산 관리 도구를 제공합니다. 우리의 핵심 전문 지식은 에너지, 광업 및 하이테크 시장에 집중되어 있습니다. 우리는 주요 국제 기업과 협력하고 있습니다. 고객의 30%가 해당 분야에서 최고의 성과를 거두고 있습니다.",
+        "en": "We have a comprehensive offering of asset management tools developed for a wide range of industries. Our core expertise is concentrated on the energy, mining and high-tech markets. We work with major international companies — 30% of our clients are top performers in their sectors."
+    },
+    "overview-title2": {
+        "kr": "위치",
+        "en": "Locations"
+    },
+    "overview-des2": {
+        "kr": "당사의 비즈니스 지역은 아프리카, 아시아, 북부, 중부 및 동부 유럽을 포괄합니다. 우리는 핀란드, 중국, 한국, 남아프리카 공화국, 탄자니아, 영국, 슬로바키아, 터키, 스위스, 네덜란드, 싱가포르, 말레이시아에서 프로젝트를 개발합니다.",
+        "en": "Our business geography covers Africa, Asia, Northern, Central, and Eastern Europe. We develop projects in Finland, China, the Republic of Korea, South Africa, Tanzania, the UK, Slovakia, Turkey, Switzerland, Netherlands, Singapore, and Malaysia."
+    },
+    "services": {
+        "kr": "서비스",
+        "en": "Services"
+    },
+    "service-title1": {
+        "kr": "예술 작품으로 승화시킨 컨설팅",
+        "en": "Consulting into a work of art"
+    },
+    "service-des1": {
+        "kr": "Prime IT Oy는 컨설팅을 예술 작품으로 승화시킵니다. 우리는 각 프로젝트에 대한 창의적인 접근 방식을 찾고, 누락된 다리를 건설하며 성공을 위한 모든 요소가 함께 작동하도록 합니다.",
+        "en": "Prime IT Oy turns consulting into a work of art. We find a creative approach to each project, build the missing bridges and make all the ingredients for success work together."
+    },
+    "service-title2": {
+        "kr": "다양한 전문적인 서비스",
+        "en": "Diverse expert services"
+    },
+    "service-des2": {
+        "kr": "당사는 전략적 분석, 국제 사업 개발, 투자 유치, 수혜자 이익 대변, 경영 컨설팅 및 기타 전문 서비스를 제공합니다.",
+        "en": "We offer strategic analysis, international business development, raising investment, beneficiary interest representation, management consulting and other expert services."
+    },
+    "service-title3": {
+        "kr": "복잡한 국제 협상을 성공적으로 지원",
+        "en": "Support the most complex negotiations"
+    },
+    "service-des3": {
+        "kr": "위기 관리 및 기업 갈등 해결에 대한 광범위한 경험을 통해 우리는 비교문화적 커뮤니케이션의 뉘앙스와 함정을 고려하여 가장 복잡한 국제 협상을 성공적으로 지원합니다.",
+        "en": "Extensive experience in crisis management and resolving corporate conflicts allows us to successfully support the most complex international negotiations, taking into account cross-cultural communication nuances and pitfalls."
+    },
+    "values": {
+        "kr": "가치",
+        "en": "Values"
+    },
+    "value-title1": {
+        "kr": "우리는 모든 약속을 이행합니다.",
+        "en": "We deliver on all our commitments."
+    },
+    "value-des1": {
+        "kr": "우리가 프로젝트를 맡으면 이해관계자에게 최대한의 이익을 제공하기 위해 우리가 할 수 있는 모든 일을 합니다.",
+        "en": "If we take on a project, we do everything within our power to offer the maximum benefit to the stakeholders."
+    },
+    "value-title2": {
+        "kr": "우리는 공동체의 힘을 믿습니다.",
+        "en": "We believe in the power of community."
+    },
+    "value-des2": {
+        "kr": "우리 작업의 중요한 부분은 광범위한 네트워킹을 기반으로 합니다. 우리 앞에 문은 언제나 열려 있습니다.",
+        "en": "An important part of our work is based on extensive networking. The doors that open before us always remain open."
+    },
+    "value-title3": {
+        "kr": "우리는 환경 지속 가능성에 전념합니다.",
+        "en": "We are committed to environmental sustainability."
+    },
+    "value-des3": {
+        "kr": "Prime IT Oy는 환경 보호와 자연과 인간의 균형 잡힌 상호 작용에 의미있는 기여를 하기 위해 분투합니다.",
+        "en": "Prime IT Oy strives to make a significant contribution to environmental protection and the balanced interaction between nature and mankind."
+    },
+    "value-title4": {
+        "kr": "우리는 공정하게 플레이합니다.",
+        "en": "We play fair."
+    },
+    "value-des4": {
+        "kr": "우리는 윤리적인 비즈니스 관행을 통해 명성을 얻었습니다. 우리는 지역 사회에 해를 끼칠 수 있는 프로젝트를 하지 않습니다.",
+        "en": "We have earned our reputation through ethical business practices. We don't do projects that could harm local communities."
+    },
+    "value-title5": {
+        "kr": "우리는 사회적 책임이 있습니다.",
+        "en": "We are socially responsible."
+    },
+    "value-des5": {
+        "kr": "우리는 우리가 하는 일이 지역 사회에 힘을 실어주고 사회적 장벽을 제거함으로써 사회 복지에 도움이 될 수 있다고 믿습니다.",
+        "en": "We believe our work can benefit the welfare of society by re-empowering local communities and eliminating social barriers."
+    },
+    "client": {
+        "kr": "고객의 눈으로 본 우리의 장점",
+        "en": "Our advantages through the eyes of clients"
+    },
+    "client-detail1": {
+        "kr": "탁월한 네트워킹 능력. 단순히 적임자를 찾아 유능한 프레젠테이션을 하는 것 뿐만 아니라 모두가 듣고 이해할 수 있도록 섬세하고 정성적으로 입장을 전달합니다.",
+        "en": "Excellent networking capabilities. Not just finding the number of the right person and making a competent presentation, but subtly and qualitatively conveying the position so that everyone listens and understands."
+    },
+    "client-detail2": {
+        "kr": "가장 비인습적인 업무를 셋업하고 잊어버릴 수도 있습니다… 모든것이 완료되어 있을 겁니다",
+        "en": "The most unconventional task can be set and forgotten... Everything will be done."
+    },
+    "client-detail3": {
+        "kr": "가장 어려운 작업을 한 헤드에서 다른 쪽으로 옮기고 착지하여 가시적인 결과를 가져오는 능력입니다.",
+        "en": "The ability to transfer the toughest tasks from one head to another, land them and bring them to a tangible result."
+    },
+    "client-detail4": {
+        "kr": "올바른 우선 순위를 설정하고 원하는 결과를 달성하는 명확성, 진정성 및 숙련도.",
+        "en": "Clarity, integrity, and proficiency in setting the right priorities and achieving the desired results."
+    },
+    "client-detail5": {
+        "kr": "모든 것을 제 시간에, 예산에 맞게, 적절한 범위내에서 해낼 뿐만 아니라 조건이 없을 때에도 모든 것을 자율적으로 조정해서 실행해냅니다.",
+        "en": "Not just doing everything on time, on budget and to the right extent, but arranging everything autonomously when no conditions are in place."
+    },
+    "client-detail6": {
+        "kr": "마치 공기처럼 모든 것을 하나로 모을 수 있습니다.",
+        "en": "Can put everything together — just as the air does."
+    },
+    "contact": {
+        "kr": "문의하기",
+        "en": "Contact"
+    },
+    "send": {
+        "kr": "메세지 보내기",
+        "en": "Send Message"
+    },
+    "location": {
+        "kr": "위치",
+        "en": "Location"
+    },
+    "location-head": {
+        "kr": "본사 | 에스포, 핀란드",
+        "en": "Head office | Espoo, Finland"
+    },
+    "location-rep": {
+        "kr": "대표사무소 | 대한민국 서울",
+        "en": "Representative office | Seoul, Korea"
+    },
+    "call": {
+        "kr": "연락처",
+        "en": "Call"
+    },
+    "call-head": {
+        "kr": "본사 |",
+        "en": "Head office | +358"
+    },
+    "call-rep": {
+        "kr": "대표사무소 | ",
+        "en": "Representative office | +82"
+    },
+    "email": {
+        "kr": "이메일",
+        "en": "Email"
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+    "": {
+        "kr": "",
+        "en": ""
+    },
+
+}
